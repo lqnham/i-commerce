d23841576fcdad6d74d51a882d55e1bd31441915
nhamlq <nham.le@axonactive.com>
2020-06-22 13:20:30 +0700

    handle eureka

--- iorder/src/main/java/com/icommerce/iorder/IOrderApplication.java
+++ iorder/src/main/java/com/icommerce/iorder/IOrderApplication.java
@@ -2,8 +2,11 @@ package com.icommerce.iorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class IOrderApplication {
 
     public static void main(String[] args) {
--- iproduct/src/main/java/com/icommerce/iproduct/IProductApplication.java
+++ iproduct/src/main/java/com/icommerce/iproduct/IProductApplication.java
@@ -3,13 +3,15 @@ package com.icommerce.iproduct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.time.LocalDateTime;
 
 @Slf4j
 @SpringBootApplication
-//@EnableEurekaClient
-//@EnableFeignClients
+@EnableEurekaClient
+@EnableFeignClients
 public class IProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(IProductApplication.class, args);
